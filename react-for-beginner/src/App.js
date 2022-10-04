61167046ee2d041cd7df7bda0d12bcc64b9614e6
chaeeunj <official2930@naver.com>
2022-10-05 01:37:52 +0900

    7.5 React Router

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 import Movie from './components/Movie';
 
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
